c0cde6525fafb13f7a636817d43d8779b0bcdf38
Hossam Shaher <hosamshaher43@gmail.com>
2026-02-28 22:48:48 +0200

    Update FIFO.sv

--- Part 3: Digital Building Blocks/3. Memory Arrays/FIFO.sv
+++ Part 3: Digital Building Blocks/3. Memory Arrays/FIFO.sv
@@ -1,4 +1,3 @@
-// Code your design here
 //2**N-word (depth) × M-bit (width) FIFO buffer
 //Inputs:
 //	clk, reset_n, re (read enable), we (write enable), wd (write data)
@@ -29,28 +28,31 @@ module FIFO
   
   logic [N:0] w_ptr, r_ptr;
   
-  always_ff @(posedge clk)
+  always @(posedge clk)
     if(!reset_n) begin
-      empty = 1;
-      full 	= 0;
-      w_ptr = 0; 
-      r_ptr	= 0; 
+      empty <= 1;
+      full 	<= 0;
+      w_ptr <= 0; 
+      r_ptr	<= 0; 
     end
     else begin
       //write data
       if(we && !full) begin
-        fifo[w_ptr] = wd;
-        w_ptr++;
+        fifo[w_ptr] <= wd;
+        w_ptr <= w_ptr + 1;
       end
       //read data
       if(re && !empty) begin
-        rd = fifo[r_ptr];
-        r_ptr++;
+        rd <= fifo[r_ptr];
+        r_ptr <= r_ptr + 1;
       end
-      //empty flag
-      empty = (r_ptr === w_ptr);
-      //full flag
-      full 	= (r_ptr[N-1:0] === w_ptr[N-1:0]) && (r_ptr[N] !== w_ptr[N]);
     end
+  
+  always @(*) begin
+    //empty flag
+    empty = (r_ptr === w_ptr);
+    //full flag
+    full = (r_ptr[N-1:0] === w_ptr[N-1:0]) && (r_ptr[N] !== w_ptr[N]);
+  end
  
 endmodule: FIFO
