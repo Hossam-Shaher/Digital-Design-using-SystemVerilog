e67b504e49ae9514e7c4afba756ad5c5d0be9c1c
Hossam Shaher <hosamshaher43@gmail.com>
2026-03-01 14:19:44 +0200

    Update fsm_mxxxx_101_detector_tb.sv

--- Part 2: Sequential Logic Design/fsm_mxxxx_101_detector_tb.sv
+++ Part 2: Sequential Logic Design/fsm_mxxxx_101_detector_tb.sv
@@ -3,46 +3,46 @@
 
 module fsm_mxxxx_101_detector_tb;
 
-  timeunit 1ns; timeprecision 1ps;
-  
-  localparam half_cycle = 3;
+  //timeunit 1ns; timeprecision 1ps;
+
+  localparam 	half_cycle = 3;
   var  logic 	clk, reset_n, 
-  				x; 
+                x; 
   wire logic 	y_mealy, y_moore;
-  
+
   fsm_mealy_101_detector mealy (.*, .y(y_mealy)); 
   fsm_moore_101_detector moore (.*, .y(y_moore)); 
-  
+
   always #half_cycle clk = ~clk;
-  
+
   initial begin
 
-    @(posedge clk) #1ps x = 1;
-    @(posedge clk) #1ps x = 0;
-    @(posedge clk) #1ps x = 0;
-    @(posedge clk) #1ps x = 1;
-    @(posedge clk) #1ps x = 0;
-    @(posedge clk) #1ps x = 1;	//101
-    @(posedge clk) #1ps x = 0;
-    @(posedge clk) #1ps x = 1;	//101
-    @(posedge clk) #1ps x = 1;
-    @(posedge clk) #1ps x = 0;
-    @(posedge clk) #1ps x = 1;	//101
-  
+    @(posedge clk) x <= 1;
+    @(posedge clk) x <= 0;
+    @(posedge clk) x <= 0;
+    @(posedge clk) x <= 1;
+    @(posedge clk) x <= 0;
+    @(posedge clk) x <= 1;	//101
+    @(posedge clk) x <= 0;
+    @(posedge clk) x <= 1;	//101
+    @(posedge clk) x <= 1;
+    @(posedge clk) x <= 0;
+    @(posedge clk) x <= 1;	//101
+
     #(4*half_cycle) $finish;
-    
-  end
-  
-  initial begin
-    clk = 0;
-    x = 0;
 
-    reset_n = 1;
-    #1 reset_n = 0;
-    #1 reset_n = 1;
-    
+  end
+
+  initial begin
+    clk <= 0;
+    x <= 0;
+
+    reset_n <= 1;
+    #1 reset_n <= 0;
+    #1 reset_n <= 1;
+
     $dumpfile("dump.vcd");
     $dumpvars;
   end
-  
+
 endmodule: fsm_mxxxx_101_detector_tb
